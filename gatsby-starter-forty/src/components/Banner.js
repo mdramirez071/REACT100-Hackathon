221e65dce2ddb7f18f1839b5dcc2356aa3003168
mdramirez071 <mdramirez071@gmail.com>
2019-11-30 11:11:37 -0800

    updating portfolio site

---
+++ gatsby-starter-forty/src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Hi, my name is Michael.</h1>
+            </header>
+            <div className="content">
+                <p>Welcome to my portfolio and please feel free to contact me directly if you have any questions or opportunities.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Check Out My Projects</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
